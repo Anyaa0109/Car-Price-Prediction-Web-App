5f98e56b0c9e56e910993518637a07e04c7d429f
Ananya Gupta <105000516+Anyaa0109@users.noreply.github.com>
2025-03-24 18:12:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,5 @@ def predict():
     # Render template with prediction
     return render_template('index.html', prediction=round(predicted_price, 2))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
